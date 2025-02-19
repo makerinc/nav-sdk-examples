11b998b25da78dca98a7bbb744ba31242693fe07
Naman Sood <54349762+namansoood@users.noreply.github.com>
2025-02-19 20:29:40 +0530

    wip

--- src/components/MyCustomProductCard.tsx
+++ src/components/MyCustomProductCard.tsx
@@ -10,8 +10,13 @@ export const Component = (props: Props) => {
 			<div>
 				{props.data.name}
 			</div>
+			<div>
+				{props.data.otherVariants[0].price}
+			</div>
 		</div>
 	)
 }
 
 registry.register('product', "my-custom-product-card", Component);
+
+export default Component;
