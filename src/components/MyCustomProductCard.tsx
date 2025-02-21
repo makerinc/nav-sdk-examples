042c5c7599228fcc6cbfc08931ecc1d96633c80c
Naman Sood <54349762+namansoood@users.noreply.github.com>
2025-02-21 20:04:07 +0530

    test out the new image component

--- src/components/MyCustomProductCard.tsx
+++ src/components/MyCustomProductCard.tsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
-import { Product, registry } from 'nav-sdk';
+
+import { Product, registry, Image } from 'nav-sdk';
 
 type Props = {
 	data: Product;
 }
 
 export const Component = (props: Props) => {
-	let [count, setCount] = useState(0);
 	return (
-		<div>
+		<div style={{
+			display: 'flex',
+			flexDirection: 'column',
+		}}>
+			<div style={{
+				position: 'relative',
+				width: '100%',
+				paddingBottom: '100%',
+			}}>
+				<Image src="https://placekitten.com/200/300" alt='Kitten' fit="cover" priority={0} />
+
+			</div>
 			<div>ID: <b>{props.data.id}</b></div>
 			<div>
 				Name: <b>{props.data.name}</b>
-			</div>
-			<div>
-				<button onClick={() => setCount(count + 1)}>
-					Clicked {count} times
-				</button>
 			</div>
 		</div>
 	)
